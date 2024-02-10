e068feb3ab0eebcfd737d6789b344879a70f1b1f
Purushottam <131853935+ProElecttro@users.noreply.github.com>
2024-02-10 18:19:02 +0530

    built

--- dist/entities/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const notes_1 = require("./notes");
-const user_1 = require("./user");
-const Table = [notes_1.Note, user_1.User];
-exports.default = Table;
